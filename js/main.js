732d38bd9404c812f1540404bf3d1d3c2676d4a9
l <lorenzo.regalzi@gmail.com>
2020-02-20 14:25:00 +0100

    inizio

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+// var numero = 12
+// var paolo = 3
+//
+// var risultato = numero% paolo
+// console.log(risultato);
+
+
+
+  for (var i = 1; i <= 100; i++) {
+     console.log(i);
+//     var tre = i % 3;
+//     if (tre) {
+//         console.log('mario');
+//     }
+ }
